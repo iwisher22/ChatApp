cb6061d77f070acdda72c9ea81b03eaeee00cc98
iwisher22 <ashusingh9559299029@gmail.com>
2024-02-16 23:55:51 +0530

    fd

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
 	return (
@@ -6,7 +6,7 @@ const Login = () => {
 			<div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
 				<h1 className="text-3xl font-semibold text-center text-gray-300">
 					Login
-					<span className="text-blue-500">ChatApp</span>
+					<span className="text-blue-500"> ChatApp</span>
 				</h1>
 				<form>
 					<div>
@@ -29,11 +29,11 @@ const Login = () => {
 							className="w-full input input-bordered h-10"
 						/>
 					</div>
-					<a
-						href="/signup"
+					<Link
+						to="/signup"
 						className="text-sm  hover:underline hover:text-blue-600 mt-2 inline-block">
 						{"Don't"} have an account?
-					</a>
+					</Link>
 					<div>
 						<button className="btn btn-block btn-sm mt-2">Login</button>
 					</div>
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,6 +1,27 @@
+import { Link } from "react-router-dom";
 import GenderCheckBox from "./GenderCheckbox";
+import { useState } from "react";
+import UseSignup from "../../hooks/useSignup";
 
 const SignUp = () => {
+	const [inputs, setInputs] = useState({
+		fullName: "",
+		username: "",
+		password: "",
+		confirmPassword: "",
+		gender: "",
+	});
+
+	const { loading, signup } = UseSignup();
+
+	const handleCheckboxChange = (gender) => {
+		setInputs({ ...inputs, gender });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await signup(inputs);
+	};
 	return (
 		<div className="flex flex-col items-center justify-center min-w-96 mx-auto">
 			<div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -8,7 +29,7 @@ const SignUp = () => {
 					Sign Up
 					<span className="text-blue-500"> ChatApp</span>
 				</h1>
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div>
 						<label className="label p-2">
 							<span className="text-base label-text">Full Name</span>
@@ -17,6 +38,10 @@ const SignUp = () => {
 							type="text"
 							placeholder="Ashu Singh"
 							className="w-full input input-bordered h-10"
+							value={inputs.fullName}
+							onChange={(e) =>
+								setInputs({ ...inputs, fullName: e.target.value })
+							}
 						/>
 					</div>
 					<div>
@@ -27,6 +52,10 @@ const SignUp = () => {
 							type="text"
 							placeholder="iwisher"
 							className="w-full input input-bordered h-10"
+							value={inputs.username}
+							onChange={(e) =>
+								setInputs({ ...inputs, username: e.target.value })
+							}
 						/>
 					</div>
 					<div>
@@ -37,6 +66,10 @@ const SignUp = () => {
 							type="password"
 							placeholder="password"
 							className="w-full input input-bordered h-10"
+							value={inputs.password}
+							onChange={(e) =>
+								setInputs({ ...inputs, password: e.target.value })
+							}
 						/>
 					</div>
 					<div>
@@ -47,16 +80,31 @@ const SignUp = () => {
 							type="password"
 							placeholder="confirm password"
 							className="w-full input input-bordered h-10"
+							value={inputs.confirmPassword}
+							onChange={(e) =>
+								setInputs({ ...inputs, confirmPassword: e.target.value })
+							}
 						/>
 					</div>
-					<GenderCheckBox />
-					<a
+					<GenderCheckBox
+						onCheckboxChange={handleCheckboxChange}
+						selectedGender={inputs.gender}
+					/>
+					<Link
 						className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
-						href="#">
+						to="/login">
 						Already have an account?
-					</a>
+					</Link>
 					<div>
-						<button className="btn btn-block btn-sm mt-2">Sign Up</button>
+						<button
+							className="btn btn-block btn-sm mt-2 border border-slate-700"
+							disabled={loading}>
+							{loading ? (
+								<span className="loading loading-spinner"></span>
+							) : (
+								"Sign Up"
+							)}
+						</button>
 					</div>
 				</form>
 			</div>
